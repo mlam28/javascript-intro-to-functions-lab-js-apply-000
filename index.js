f339ed6c3308723560672b1c939f880e71c0ce30
Michelle Lam <shelly2.lam@gmail.com>
2019-05-21 14:45:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if(string.toLowerCase()) {
-    console.log("I can't hear you!")
+  if(string.toLowerCase() === lowercase) {
+    return "I can't hear you!"
   }
   else if(string.toUpperCase()) {
     console.log( "YES INDEED!")
